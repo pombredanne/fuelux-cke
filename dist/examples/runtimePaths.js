1f727e9f94642ac6050fa42ac7bf94d47adc5c64
kevinparkerson <kparkerson@exacttarget.com>
2013-03-12 17:49:53 -0400

    removing what's in here

--- dist/examples/runtimePaths.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        'ckeditor': 'http://dl.dropbox.com/u/12146987/ckeditor4/ckeditor',
-        'fuelux-ckeditor': './',
-        'jquery': '../lib/jquery'
-    }
-});
