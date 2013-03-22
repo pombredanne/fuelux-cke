dada90c966dfd8c1ed97e93f29af12adff6aeddd
kevinparkerson <kparkerson@exacttarget.com>
2013-03-22 12:27:42 -0400

    updating Gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,207 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    //Configuration
+
+    grunt.initConfig({
+//        clean: {
+//            dist: ['dist/'],
+//            docs: ['dist/docs/'],
+//            final: ['dist/build.txt']
+//        },
+//        compress: {
+//            zip: {
+//                files: [
+//                    {
+//                        cwd: 'dist/',
+//                        expand: true,
+//                        src: ['**']
+//                    }
+//                ],
+//                options: {
+//                    archive: 'dist/fuelux-editor.zip',
+//                    mode: 'zip'
+//                }
+//            }
+//        },
+//        copy: {
+//            docs: {
+//                expand: true,
+//                cwd: 'docs/themes/',
+//                src: ['**'],
+//                dest: 'dist/docs/themes/'
+//            },
+//            images: {
+//                expand: true,
+//                cwd: 'src/img/',
+//                src: ['**'],
+//                dest: 'dist/img/'
+//            }
+//        },
+//        jshint: {
+//            options: {
+//                boss: true,
+//                browser: true,
+//                curly: false,
+//                eqeqeq: true,
+//                eqnull: true,
+//                globals: {
+//                    define: true,
+//                    jQuery: true,
+//                    require: true
+//                },
+//                immed: true,
+//                latedef: true,
+//                newcap: true,
+//                noarg: true,
+//                smarttabs: true,
+//                sub: true,
+//                undef: true
+//            },
+//            source: ['Gruntfile.js', 'src/**/*.js'],
+//            tests: {
+//                options: {
+//                    undef: false,
+//                    unused: false,
+//                    latedef: false
+//                },
+//                files: {
+//                    src: ['test/**/*.js']
+//                }
+//            }
+//        },
+//        pkg: grunt.file.readJSON('package.json'),
+//        qunit: {
+//            /*full: {
+//             options: {
+//             urls: '<%= testUrls %>'
+//             }
+//             },*/
+//            simple: ['test/**/*.html']
+//        },
+//        recess: {
+//            compile: {
+//                dest: 'dist/css/fuelux-editor.css',
+//                options: {
+//                    compile: true
+//                },
+//                src: ['src/less/fuelux-editor.less']
+//            },
+//            compress: {
+//                dest: 'dist/css/fuelux-editor.min.css',
+//                options: {
+//                    compile: true,
+//                    compress: true
+//                },
+//                src: ['src/less/fuelux-editor.less']
+//            }
+//        },
+//        requirejs: {
+//            combine: {
+//                options: {
+//                    appDir: 'src',
+//                    baseUrl: '.',
+//                    dir: 'dist',
+//                    keepBuildDir: true,
+//                    modules: [
+//                        {
+//                            name: 'fuelux-editor/all'
+//                        }
+//                    ],
+//                    normalizeDirDefines: 'all',
+//                    optimize: 'none',
+//                    optimizeCss: 'none',
+//                    paths: {
+//                        'fuelux-editor': '../dist'
+//                    },
+//                    wrap: true
+//                }
+//            }
+//        },
+        shell: {
+            devsetup: {
+                command: grunt.file.read('util/devsetup.sh'),
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true
+                }
+            },
+            mkdist: {
+                command: 'mkdir dist'
+            },
+            updatelib: {
+                command: grunt.file.read('util/updatelib.sh'),
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true
+                }
+            }
+        }
+//        uglify: {
+//            options: {
+//                banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+//                    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+//                    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+//                    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+//                    ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+//            },
+//            all: {
+//                files: {
+//                    'dist/all.min.js': ['dist/all.js']
+//                }
+//            }
+//        },
+//        watch: {
+//            files: ['Gruntfile.js', 'lib/**', 'src/**', 'test/**'],
+//            tasks: ['lint', 'qunit', 'recess']
+//        },
+//        yuidoc: {
+//            compile: {
+//                name: '<%= pkg.title || pkg.name %>',
+//                description: '<%= pkg.description %>',
+//                options: {
+//                    linkNatives: true,
+//                    outdir: 'dist/docs',
+//                    paths: 'src'
+//                },
+//                url: '<%= pkg.homepage %>',
+//                version: '<%= pkg.version %>'
+//            }
+//        }
+    });
+
+    //Plugins
+
+    //grunt.loadNpmTasks('grunt-contrib-clean');
+    //grunt.loadNpmTasks('grunt-contrib-compress');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-qunit');
+    //grunt.loadNpmTasks('grunt-contrib-requirejs');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-contrib-yuidoc');
+    //grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-shell');
+
+    //Tasks
+
+    //grunt.registerTask('default', ['fulltest', 'clean:dist', 'shell:mkdist', 'requirejs', 'uglify', 'fullcss', 'copy:images', 'docs', 'compress', 'clean:final']);
+    grunt.registerTask('default', ['updatelib']);
+
+    //grunt.registerTask('devserver', ['quicktest', 'quickcss', 'watch']);
+    //grunt.registerTask('devsetup', ['shell:devsetup']);
+
+    //grunt.registerTask('docs', ['clean:docs', 'yuidoc', 'copy:docs']);
+
+    //grunt.registerTask('fullcss', ['quickcss', 'recess:compress']);
+    //grunt.registerTask('quickcss', ['recess:compile']);
+
+    //grunt.registerTask('fulltest', ['jshint', 'qunit:simple']);
+    //grunt.registerTask('quicktest', ['jshint', 'qunit:simple']);
+
+    grunt.registerTask('updatelib', ['shell:updatelib']);
+
+};
--- util/updatelib.sh
+++ util/updatelib.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# set the current directory to this file's parent, then move up to fuelux-ckeditor
-cd "$(dirname "$0")"
-cd ../
-
 # use volo to force add all base libraries (order is important)
 volo add jrburke/requirejs -f
 volo add jquery/jquery -f
