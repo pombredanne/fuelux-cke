42347c7135f4d423c5a0eeed762c19473717cacd
kevinparkerson <kparkerson@exacttarget.com>
2013-03-22 19:57:35 -0400

    adding examples

--- util/updatelib.sh
+++ util/updatelib.sh
@@ -2,7 +2,6 @@
 
 # use volo to force add all base libraries
 volo add ckeditor/ckeditor-dev -f
-volo add cloudhead/less.js/#dist/less-1.3.3.js -f
 volo add jquery/jquery -f
 volo add jquery/qunit/#qunit/ -f
 volo add jrburke/requirejs -f
