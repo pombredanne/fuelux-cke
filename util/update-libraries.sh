c666d8c27cb0c75a114df784ea51ff08b81f20be
kevinparkerson <kparkerson@exacttarget.com>
2013-03-22 12:10:15 -0400

    updating libs and dev index

--- util/update-libraries.sh
+++ util/update-libraries.sh
@@ -7,7 +7,10 @@ cd ../
 # use volo to force add all base libraries (order is important)
 volo add jrburke/requirejs -f
 volo add jquery/jquery -f
+volo add jquery/qunit/#qunit/ -f
 volo add ckeditor/ckeditor-dev -f
+volo add cloudhead/less.js/#dist/less-1.3.3.js -f
+volo add kevinparkerson/requirejs-aurl -f
 
 # build ckeditor
 chmod +x ./lib/ckeditor-dev/dev/builder/build.sh
