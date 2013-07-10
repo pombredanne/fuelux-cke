3e65be76631feafbf3fb164e35ce661cc3ec43d3
Scott Plumlee <scott@plumlee.org>
2013-07-10 13:40:46 -0400

    Correct broken CKEditor files, and finally change config SCAYT to QA.

--- dist/config.js
+++ dist/config.js
@@ -32,8 +32,8 @@ define(['require','fuelux-cke/ckeditor/ckeditor','fuelux-cke/dir'],function (req
 
 
     //DEFAULT SETTINGS
-
-	config.scayt_srcUrl = 'https://app.s1.exct.net/spellcheck/lf/scayt/scayt.js';
+	config.scayt_srcUrl = 'http://qanv1spell01.qa.local/spellcheck/lf/scayt/scayt.js';
+	// config.scayt_srcUrl = 'https://app.s1.exct.net/spellcheck/lf/scayt/scayt.js';
     //config.scayt_customerid = '1:TP9O44-I3npX3-BzRrT3-phukM-Z3MLc2-C54oN1-4dqFI2-csepO1-SJ2yh1-eGvAg4';
 	config.scayt_autoStartup = true;
 	config.scayt_maxSuggestions = 4;
