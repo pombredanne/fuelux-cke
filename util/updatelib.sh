fa9c6864072d5721b8bf74d19965ca4f65330a84
Scott Plumlee <scott@plumlee.org>
2013-07-10 13:36:53 -0400

    Revert "Update CKEditor and build."
    This reverts commit 0f7be0cfbb00392ed9bfb17f07c8d79527c9cefb.

--- util/updatelib.sh
+++ util/updatelib.sh
@@ -2,9 +2,9 @@
 
 # use volo to force add all base libraries
 volo add ckeditor/ckeditor-dev -f
-volo add jquery/jquery/1 -f
+volo add jquery/jquery -f
 volo add jquery/qunit/#qunit/ -f
-volo add jrburke/requirejs/2.1.5 -f
+volo add jrburke/requirejs -f
 
 # build ckeditor
 chmod +x ./lib/ckeditor-dev/dev/builder/build.sh
